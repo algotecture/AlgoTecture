b6b8669e43913845c3c7861f9e3ef2e66d687d91
ssipakov <ssipakov@PC0019>
2023-03-06 17:04:20 +0300

    removed HintPath in csproj files

--- AlgoTecture.EfCli/Program.cs
+++ AlgoTecture.EfCli/Program.cs
@@ -5,17 +5,20 @@ namespace AlgoTecture.EfCli
 {
     class Program
     {
-        private static IConfiguration configuration;
+        private static IConfiguration? _configuration;
 
         static async Task<int> Main(string[] args)
         {
             Console.WriteLine("EfCli has been started");
             var currentDirectory = Directory.GetCurrentDirectory();
-            configuration = new ConfigurationBuilder().SetBasePath(currentDirectory)
+            _configuration = new ConfigurationBuilder().SetBasePath(currentDirectory)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var connectionString = _configuration?.GetConnectionString("DefaultConnection");
+            if (connectionString == null) return 0;
+            
             var context = new ApplicationDbContext(connectionString);
             await context.Database.EnsureCreatedAsync();
+
             return 0;
         }
     }
