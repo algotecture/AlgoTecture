a6c1e0d2f90579d44ae9a901a356e657a4dcf715
ssipakov <ssipakov@PC0019>
2023-03-16 18:29:09 +0300

    added test folder and project

---
+++ AlgoTecture.Libraries.Reservations.Tests/ReservationTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace AlgoTecture.Libraries.Reservations.Tests;
+
+public class ReservationTests
+{
+    [Test]
+    public void Assert_That_Updated_Properties_Are_Correct()
+    {
+        
+    }
+}
