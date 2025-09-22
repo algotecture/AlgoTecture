2fc6099c487a2cdd24c076564814e7e27701aee2
sipakov <sipakov1987@gmail.com>
2025-09-22 12:47:17 +0300

    uploaded image and seeded data

--- AlgoTecture.Libraries.GeoAdminSearch/GeoAdminSearcher.cs
+++ AlgoTecture.Libraries.GeoAdminSearch/GeoAdminSearcher.cs
@@ -12,7 +12,7 @@ namespace AlgoTecture.Libraries.GeoAdminSearch
     {
         public async Task<IEnumerable<Attrs>> GetAddress(string term)
         {
-            var baseUrl = $"https://api3.geo.admin.ch/rest/services/api/SearchServer?searchText={term}&type=locations&origins=address&limit=5";
+            var baseUrl = $"https://api3.geo.admin.ch/rest/services/api/SearchServer?searchText={term}&type=locations&origins=address&limit=7";
 
             var responseFromServer = await HttpWebRequestAssistant.GetResponse(baseUrl);
 
